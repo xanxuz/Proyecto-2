0e5418e1311d4148b54cfea4ea1fb9cded4f8617
Erick Rincones <rebaya17@gmail.com>
2016-11-08 11:02:30 -0400

    Nuevas pruebas :D

--- main.c
+++ main.c
@@ -2,107 +2,51 @@
 
 #include "custom_malloc.h"
 
-void memcheck (void * ptr) {
-	BLOCK * block = (BLOCK *)ptr - 1;
-	int cont = 1;
-	
-	while (block) {
-		printf("%2d) 0x%X -> ", cont++, DIR(block));
-		NEXT ? printf("0x%X", DIR(NEXT)) : printf("NULL\t");
-		printf("\t%9lu B", SIZE);
-		FREE ? printf(" (*)\n") : printf("\n");
-		
-		block = NEXT;
-	}
-}
-
 int main(void) {
-	int i, * array1, * array2, * array3, * array4;
-	cfree(NULL);
-	printf("\n");
-	print_free_list();
-	printf("\n\n");
-	
-	array1 = (int *) cmalloc(30 * sizeof(int));
-	for (i = 0; i < 30; i++) array1[i] = 1;
-	
-	array2 = (int *) ccalloc(20, sizeof(int));
-	for (i = 0; i < 20; i++) array2[i] = 2;
-	
-	array3 = (int *) ccalloc(10, sizeof(int));
-	for (i = 0; i < 10; i++) array3[i] = 3;
-	
-	array4 = (int *) cmalloc(15 * sizeof(int));
-	for (i = 0; i < 15; i++) array4[i] = 4;
-	
-	
-	
-	printf("\n");
-	printf("array1"); for (i = 0; i < 30; i++) printf(" %d", array1[i]); printf("\n");
-	printf("array2"); for (i = 0; i < 20; i++) printf(" %d", array2[i]); printf("\n");
-	printf("array3"); for (i = 0; i < 10; i++) printf(" %d", array3[i]); printf("\n");
-	printf("array4"); for (i = 0; i < 15; i++) printf(" %d", array4[i]); printf("\n");
-	
-	printf("\n");
-	memcheck(array1);
-	
-	printf("\n");
-	print_free_list();
-	printf("\n\n");
-	
-	
-	cfree(array2);
-	
-	printf("\n");
-	printf("array1"); for (i = 0; i < 30; i++) printf(" %d", array1[i]); printf("\n");
-	printf("array2"); for (i = 0; i < 20; i++) printf(" %d", array2[i]); printf("\n");
-	printf("array3"); for (i = 0; i < 10; i++) printf(" %d", array3[i]); printf("\n");
-	printf("array4"); for (i = 0; i < 15; i++) printf(" %d", array4[i]); printf("\n");
-	
-	printf("\n");
-	memcheck(array1);
-	
-	printf("\n");
-	print_free_list();
-	printf("\n\n");
-	
-	
-	array3 = (int *) crealloc(array3, 25 * sizeof(int));
-	
-	printf("\n");
-	printf("array1"); for (i = 0; i < 30; i++) printf(" %d", array1[i]); printf("\n");
-	printf("array2"); for (i = 0; i < 20; i++) printf(" %d", array2[i]); printf("\n");
-	printf("array3"); for (i = 0; i < 25; i++) printf(" %d", array3[i]); printf("\n");
-	printf("array4"); for (i = 0; i < 15; i++) printf(" %d", array4[i]); printf("\n");
-	
-	printf("\n");
-	memcheck(array1);
-	
-	printf("\n");
-	print_free_list();
-	printf("\n\n");
-	
-	
-	cfree(array1);
-	printf("\n");
-	print_free_list();
-	printf("\n\n");
-	
-	//cfree(array2);
-	//printf("\n");
-	//print_free_list();
-	//printf("\n\n");
-	
-	cfree(array3);
-	printf("\n");
-	print_free_list();
-	printf("\n\n");
-	
-	cfree(array4);
-	printf("\n");
-	print_free_list();
-	
-	
-	
-	return EXIT_SUCCESS;
+  int i, * array1, *array2, *array3, *array4;
+  
+  array1 = (int *) ccalloc(50, sizeof(int));
+  for(i = 0; i < 50; i++) array1[i] = 1;
+  
+  array2 = (int *) cmalloc(30 * sizeof(int));
+  for(i = 0; i < 30; i++) array2[i] = 2;
+  
+  array3 = (int *) cmalloc(20 * sizeof(int));
+  for(i = 0; i < 20; i++) array3[i] = 3;
+  
+  array4 = (int *) cmalloc(25 * sizeof(int));
+  for(i = 0; i < 25; i++) array4[i] = 4;
+  
+  
+  printf("\n");
+  printf("array1:"); for(i = 0; i < 50; i++) printf(" %d", array1[i]); printf("\n");
+  printf("array2:"); for(i = 0; i < 30; i++) printf(" %d", array2[i]); printf("\n");
+  printf("array3:"); for(i = 0; i < 20; i++) printf(" %d", array3[i]); printf("\n");
+  printf("array4:"); for(i = 0; i < 25; i++) printf(" %d", array4[i]); printf("\n");
+  
+  printf("\n");
+  print_free_list();
+  
+  
+  /////////////////////////////DEAD ZONE////////////////////////////////////////////
+  cfree(array1);
+  cfree(array3);
+  
+  printf("\n");
+  print_free_list();
+  
+  
+  
+  cfree(array2);
+  
+  printf("\n");
+  print_free_list();
+  
+  printf("\n");
+  //cfree(array1);
+  //cfree(array2);
+  //cfree(array3);
+  cfree(array4);
+  
+  return EXIT_SUCCESS;
 }
